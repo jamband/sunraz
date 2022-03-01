70c155a9f6f3fc01b3d0ff93a993bdf842743ffb
Tomoki Morita <tmsongbooks215@gmail.com>
2022-03-01 20:09:43 +0900

    Improve development environment

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,12 +5,10 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 /** @type import("webpack").Configuration */
 module.exports = merge(common, {
   mode: "development",
+  watch: true,
   output: {
     filename: "[name].js",
     assetModuleFilename: "[name][ext]",
-  },
-  devServer: {
-    watchFiles: ["src/layouts/*", "src/pages/*"],
   },
   plugins: [new MiniCssExtractPlugin()],
 });
