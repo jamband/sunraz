5d78e04bb6cfe27374530efc8797fe04e438e665
Tomoki Morita <tmsongbooks215@gmail.com>
2022-03-03 02:43:36 +0900

    Fix some types for webpack configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,7 @@ const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-/** @type import("webpack").Configuration */
+/** @type {import("webpack").Configuration} */
 module.exports = merge(common, {
   mode: "development",
   watch: true,
