ac5b35ceecc779c8280774a0ca9f7335ce215fd3
Tomoki Morita <tmsongbooks215@gmail.com>
2022-11-13 23:49:04 +0900

    remove unnecessary some comments
    and use relative path for module imports

--- src/entries/app.js
+++ src/entries/app.js
@@ -1,16 +1,5 @@
 // js
-// require("bootstrap/js/dist/alert.js");
-// require("bootstrap/js/dist/button.js");
-// require("bootstrap/js/dist/carousel.js");
-require("bootstrap/js/dist/collapse.js");
-// require("bootstrap/js/dist/dropdown.js");
-// require("bootstrap/js/dist/modal.js");
-// require("bootstrap/js/dist/offcanvas.js");
-// require("bootstrap/js/dist/popover.js");
-// require("bootstrap/js/dist/scrollspy.js");
-// require("bootstrap/js/dist/tab.js");
-// require("bootstrap/js/dist/toast.js");
-// require("bootstrap/js/dist/tooltip.js");
+require("../../node_modules/bootstrap/js/dist/collapse.js");
 
 // css
 require("../styles/app.scss");
