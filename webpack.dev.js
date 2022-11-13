f25c06d6dbcbacfc177f82e7a88ae78cc88b8ccb
Tomoki Morita <tmsongbooks215@gmail.com>
2022-11-13 23:41:08 +0900

    does not use common config for webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,44 @@
-const common = require("./webpack.common");
-const { merge } = require("webpack-merge");
+const HtmlPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 /** @type {import("webpack").Configuration} */
-module.exports = merge(common, {
+module.exports = {
   mode: "development",
   watch: true,
+  entry: {
+    app: "./src/entries/app.js",
+  },
   output: {
+    path: `${__dirname}/dist`,
+    publicPath: "/",
     filename: "[name].js",
     assetModuleFilename: "[name][ext]",
   },
-  plugins: [new MiniCssExtractPlugin()],
-});
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+          { loader: "postcss-loader" },
+          { loader: "sass-loader" },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: "src/pages/index.html",
+    }),
+    new HtmlPlugin({
+      template: "src/pages/about/index.html",
+      filename: "about/index.html",
+    }),
+    new HtmlPlugin({
+      template: "src/pages/404.html",
+      filename: "404.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,45 @@
-const common = require("./webpack.common");
-const { merge } = require("webpack-merge");
+const HtmlPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 /** @type {import("webpack").Configuration} */
-module.exports = merge(common, {
+module.exports = {
   mode: "production",
+  entry: {
+    app: "./src/entries/app.js",
+  },
   output: {
+    path: `${__dirname}/dist`,
+    publicPath: "/",
     filename: "[name].[contenthash].js",
     assetModuleFilename: "[name][ext]",
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+          { loader: "postcss-loader" },
+          { loader: "sass-loader" },
+        ],
+      },
+    ],
+  },
   plugins: [
+    new HtmlPlugin({
+      template: "src/pages/index.html",
+    }),
+    new HtmlPlugin({
+      template: "src/pages/about/index.html",
+      filename: "about/index.html",
+    }),
+    new HtmlPlugin({
+      template: "src/pages/404.html",
+      filename: "404.html",
+    }),
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css",
     }),
   ],
-});
+};
