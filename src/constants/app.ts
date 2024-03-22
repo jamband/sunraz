268d4660479227c36b46c1e8b853e7eb92f98684
Tomoki Morita <tmsongbooks215@gmail.com>
2024-03-22 18:23:19 +0900

    fix project name again

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,3 +1,3 @@
-export const APP_NAME = "Sunraz" as const;
+export const APP_NAME = "SunRaz" as const;
 export const APP_DESCRIPTION =
   `${APP_NAME} is my private playground for Bootstrap.` as const;
