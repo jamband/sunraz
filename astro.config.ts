a1fa447a9c7c1f144d7b71017bd11aedc716e310
Tomoki Morita <tmsongbooks215@gmail.com>
2024-11-28 09:37:41 +0900

    silencing the deprecation warnings for sass

--- astro.config.ts
+++ astro.config.ts
@@ -3,4 +3,19 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   trailingSlash: "always",
   devToolbar: { enabled: false },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            "color-functions",
+            "global-builtin",
+            "import",
+            "legacy-js-api",
+            "mixed-decls",
+          ],
+        },
+      },
+    },
+  },
 });
